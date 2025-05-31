a2d95ae50e3331fb4b0bf4cb81a16075f851abcb
Joao Antonio <joaoantonios74@gmail.com>
2025-05-30 21:12:14 -0300

    pdf funcionando ok'

--- api/app/handlers/pedidos.py
+++ api/app/handlers/pedidos.py
@@ -3,6 +3,8 @@ from app.utils.aws import dynamodb, sqs, s3
 from app import config
 from app.schemas import PedidoInput
 from fastapi import HTTPException
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
 # def criar_pedido(pedido: PedidoInput):
 #     pedido_id = str(uuid.uuid4())
@@ -47,15 +49,23 @@ def criar_pedido(pedido: PedidoInput):
             MessageGroupId="pedidos"
         )
         
+        pdf_filename = pedido_id+".pdf"
+        c = canvas.Canvas(pdf_filename, pagesize=letter)
+        c.drawString(100, 750, f"Pedido: {pedido_id} \n")
+        c.drawString(100, 730, f"Cliente: {pedido.cliente} \n")
+        c.drawString(100, 710, f"Status: pendente\n")
+        c.drawString(100, 690, f"Mesa: {pedido.mesa}\n")
+        c.drawString(100, 670, f"Itens: {', '.join(pedido.itens)}\n")
+        c.save()
         # Log de sucesso (opcional)
-        with open("teste.txt", "a") as f:
-            f.write(f"Pedido criado com ID: ")
+        # with open("teste.txt", "a") as f:
+            # f.write(f"Pedido criado com ID: ")
         # Supondo que você tenha um cliente S3 chamado s3 no app.utils.aws
         print("antes s3")
         s3.upload_file(
-            Filename="teste.txt",
+            Filename=pdf_filename,
             Bucket=config.S3_BUCKET,
-            Key="logs/{}.txt".format(pedido_id)
+            Key="logs/{}".format(pdf_filename)
         )
         print("depois s3")
         return {"id": pedido_id, "status": "recebido"}
