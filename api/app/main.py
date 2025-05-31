5dd61ba4c6aa3cd41d1e40d1c5210fbfd9a34bb9
Joao Antonio <joaoantonios74@gmail.com>
2025-05-31 00:45:47 -0300

    preparando para o sns

--- api/app/main.py
+++ api/app/main.py
@@ -9,3 +9,8 @@ app.include_router(comprovantes_router)
 @app.post("/pedidos")
 async def post_pedido(pedido: PedidoInput):
     return criar_pedido(pedido)
+
+@app.put("/pedidos/{pedido_id}")
+async def put_pedido(pedido_id: int, pedido: PedidoInput):
+    # Aqui voce pode implementar atualização do status do pedido para "concluido"
+    return concluir_pedido(pedido_id, pedido)
